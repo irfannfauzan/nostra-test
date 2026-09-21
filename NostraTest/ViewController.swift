6d14f0ae47325cc9e0a90d97b7f8665a38adccf2
irfannfauzan <irfanfauzanrachman@gmail.com>
2026-09-21 11:24:28 +0700

    Initial Commit

---
+++ NostraTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NostraTest
+//
+//  Created by Vokal-Ican on 21/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
