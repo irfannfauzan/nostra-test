570c03f40c071be4c1b4c0920818071f6dbb7269
irfannfauzan <irfanfauzanrachman@gmail.com>
2026-09-21 13:43:08 +0700

    add ProductRepository impl calling real API

---
+++ NostraTest/Data/Repositories/ProductRepository.swift
@@ -0,0 +1,21 @@
+//
+//  ProductRepository.swift
+//  NostraTest
+//
+//  Created by Vokal-Ican on 21/09/26.
+//
+
+import Foundation
+
+final class ProductRepository: ProductRepositoryProtocol {
+    private let apiClient: APIClientProtocol
+    
+    init(apiClient: APIClientProtocol) {
+        self.apiClient = apiClient
+    }
+    
+    func fetchProducts(limit: Int, skip: Int) async throws -> [Product] {
+        let response: ProductListResponseDTO = try await apiClient.get(.productList(limit: limit, skip: skip))
+        return response.products.map { $0.toDomain() }
+    }
+}
