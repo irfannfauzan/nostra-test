d088f78e50483c6843a5b5f10add93adde882655
irfannfauzan <irfanfauzanrachman@gmail.com>
2026-09-21 12:04:40 +0700

    add models product dummy

---
+++ NostraTest/Domain/Entities/Product.swift
@@ -0,0 +1,14 @@
+//
+//  Entities.swift
+//  NostraTest
+//
+//  Created by Vokal-Ican on 21/09/26.
+//
+
+// dummy hanya untuk tampilin ui
+struct Product {
+    let title: String
+    let category: String
+    let description: String
+    let price: Double
+}
